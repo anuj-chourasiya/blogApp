1d6990e485c972e7e238ea2f3d6e6e86df3e691d
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2021-03-09 14:39:48 +0530

    changed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-'''
+
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=False)}
@@ -96,7 +96,7 @@ DATABASES = {
         'USER' : os.environ['USER'],
         'PASSWORD' : os.environ['PASSWORD']
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
